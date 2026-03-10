4145a842f8af43d152be0a35aa129690fcccadaf
Pearl <pearl@users.noreply.github.com>
2026-03-10 18:58:46 +0100

    Initial commit

--- music-control.js
+++ music-control.js
@@ -4,10 +4,8 @@ const toggleBtn = document.getElementById('music-toggle');
 // ustawiamy początkowo 0 głośności, żeby autoplay zadziałało
 music.volume = 0; 
 music.play().then(() => {
-  // jeśli uda się odpalić autoplay, ustawiamy normalną głośność po krótkiej chwili
   setTimeout(() => { music.volume = 0.3; }, 100);
 }).catch(() => {
-  // jeśli autoplay zablokowane, włączamy muzykę przy pierwszym kliknięciu
   document.body.addEventListener('click', () => { music.play(); music.volume = 0.3; }, { once: true });
 });
 
